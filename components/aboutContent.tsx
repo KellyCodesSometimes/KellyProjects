381e49bc26d4e9eb49d0673a1406f78fdfb00db4
k-a-ball <missk_a_ball@outlook.com>
2022-03-24 20:49:58 +0000

    Fix about

--- components/aboutContent.tsx
+++ components/aboutContent.tsx
@@ -5,9 +5,9 @@ export default function AboutContent() {
         <>
             <div className="logo" style={{ position: "absolute", margin: 25, textAlign: "left" }}>About</div>
             <div style={{ position: "absolute", margin: 25, marginTop: 35, textAlign: "left" }}>
-                <h3 style={{ marginTop: 50 }}>Hi, I&apos;m Kelly.</h3>
+                <h3 style={{ marginTop: 50 }}>Hi, I&apos;m Kelly, I'm a Senior Typescript Engineer.</h3>
                 <h4 style={{ fontWeight: 'bold' }}> Experience </h4>
-                <h5 style={{ fontWeight: 400 }}>&#9745; Several years&apos; experience working in tech companies.</h5>
+                <h5 style={{ fontWeight: 400 }}>&#9745; Several years&apos; working in tech companies, including a Skin Cancer Diagnostics AI company, an Autonomous Driving AI solution company, a Medications Adherence company, and an energy company.</h5>
                 <h5 style={{ fontWeight: 400 }}>&#9745; Experience doing Full Stack work and having sole/majority ownership over codebases and relevant architectural decisions </h5>
                 <h5 style={{ fontWeight: 400 }}>&#9745; Language agnostic - deep understanding of logic and able to switch languages where necessary. Largely used Ts, Js, Python, PHP, and C#. </h5>
                 <button style={{
